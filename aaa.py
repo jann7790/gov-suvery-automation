0fa6f112b5436195443e53b19d342d6b3950acca
jann7790 <jann7790@gmail.com>
2021-10-03 23:50:13 +0800

    init

---
+++ aaa.py
@@ -0,0 +1,20 @@
+import requests
+import pandas as pd
+import time
+
+questions = []
+df = pd.read_csv('qa.csv')
+try:
+    for i in range(100000):
+        try:
+            r = requests.get('https://isafeevent.moe.edu.tw/api/questions/C', headers={'UserToken': 'VXfj4HyG2UiF4vD8x6uvQ7kirDWzeD8n'})
+        except requests.exceptions.RequestException as e:
+            print(e)
+        print(r)
+        if r.status_code == 200:
+            df = pd.concat([df, pd.json_normalize(r.json()['questions'])])
+            df = df[~df.examId.duplicated()]
+        time.sleep(5)
+except KeyboardInterrupt as e:
+    print(e)
+df.sort_values('examId').to_csv('./qa.csv', index=False)
